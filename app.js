1458fbc1dc2896566fbe9b77994ae8561d058f42
sobujhstu <sobujhd@gmail.com>
2022-05-01 05:03:58 +0600

    some bug fixed

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const jwt = require('jsonwebtoken');
 function authGuard(secret_key) {
     return (req, res, next) => {
         try {
-            const auth = req.headers.authorization;
-            if (auth && auth.length > 0) {
+            const authorization = req.headers.authorization;
+            if (authorization && authorization.length > 0) {
                 // get token
-                const token = auth.split(' ')[1];
+                const token = authorization.split(' ')[1];
                 // verify token
                 const decode = jwt.verify(token, secret_key);
 
-                req.admin = decode;
+                // get user info 
+                req.user = decode;
+
                 next();
             } else {
                 res.status(401).json({
