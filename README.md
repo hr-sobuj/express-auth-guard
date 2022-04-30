197ff90985a74839241fffbe938292fdc6a8c184
sobujhstu <sobujhd@gmail.com>
2022-05-01 04:29:28 +0600

    update readme file

--- README.md
+++ README.md
@@ -1,20 +1,23 @@
 # Express Auth Guard
-
+It will help to check user are logged in or not.
 
 ## How to install
 `npm i express-auth-guard`
 
 ## How to use 
-```
-const checkAuth=require('express-auth-guard);
 
-const route.get('/',checkAuth,(req,res)=>{
+```
+const authGuard = require('express-auth-guard'); 
+
+let secret_key='slkdfjoapifjaksjasifaoidflak';
+
+router.get('/', authGuard(secret_key), (req,res)=>{
     res.statu(200).json({
-        ok:"Ok"
+        msg:"Ok"
     })
-})
-```
+});
 
+```
 
 
 Powered by https://fb.com/habibur.rahman.sobuj.bd
--- app.js
+++ app.js
@@ -2,11 +2,8 @@ const jwt = require('jsonwebtoken');
 
 function authGuard(secret_key) {
     return  (req, res, next)=>{
-        console.log("head",req.headers);
-        console.log("key",secret_key);
         try {
             const auth = req.headers.authorization;
-            console.log("auth",auth);
             if (auth && auth.length > 0) {
                 // get token
                 const token = auth.split(' ')[1];
@@ -18,13 +15,11 @@ function authGuard(secret_key) {
             } else {
                 res.status(401).json({
                     error: 'Incorrect Information!',
-                    secret_key
                 });
             }
         } catch (error) {
             res.status(401).json({
                 error: 'Authetication failure!',
-                secret_key
             });
         }
     }
